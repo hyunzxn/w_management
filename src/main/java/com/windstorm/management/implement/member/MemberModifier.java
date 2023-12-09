bfcf7c48f9fc1da916d5bf00d9bf3374079ce904
hyunzxn <jann1653@gmail.com>
2023-12-09 15:56:10 +0900

    feat: Member의 비밀번호를 변경할 수 있는 기능 구현

---
+++ src/main/java/com/windstorm/management/implement/member/MemberModifier.java
@@ -0,0 +1,22 @@
+package com.windstorm.management.implement.member;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import com.windstorm.management.controller.member.request.PasswordModify;
+import com.windstorm.management.domain.member.Member;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class MemberModifier {
+	private final PasswordEncoder passwordEncoder;
+
+	@Transactional
+	public void modifyPassword(Member member, PasswordModify request) {
+		member.modifyPassword(request);
+		member.encodePassword(passwordEncoder);
+	}
+}
