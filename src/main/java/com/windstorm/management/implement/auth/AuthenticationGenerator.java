db1f8a00f215e83e0d7bed61062e86ece48bfdd8
hyunzxn <jann1653@gmail.com>
2023-12-09 13:02:40 +0900

    refactor: MemberLoginManager에서 Authentication 객체를 만들고 인증을 해주던 것을 별도의 Component로 분리해서 처리하도록 분리

---
+++ src/main/java/com/windstorm/management/implement/auth/AuthenticationGenerator.java
@@ -0,0 +1,24 @@
+package com.windstorm.management.implement.auth;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import com.windstorm.management.controller.auth.request.Login;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationGenerator {
+	private final AuthenticationManager authenticationManager;
+
+	public Authentication authenticate(Login request) {
+		UsernamePasswordAuthenticationToken authenticationToken =
+			new UsernamePasswordAuthenticationToken(request.uniqueMemberId(), request.password());
+
+		return authenticationManager.authenticate(authenticationToken);
+	}
+
+}
