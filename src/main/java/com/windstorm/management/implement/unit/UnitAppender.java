378025778b5b3520bade1aafdbda813b2d75c5d5
hyunzxn <jann1653@gmail.com>
2023-12-13 19:40:24 +0900

    refactor: 진(Unit)에 셀(Cell)을 추가하는 책임을 UnitAppender에서 수행하도록 리팩터링

--- src/main/java/com/windstorm/management/implement/unit/UnitAppender.java
+++ src/main/java/com/windstorm/management/implement/unit/UnitAppender.java
@@ -3,8 +3,11 @@ package com.windstorm.management.implement.unit;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.windstorm.management.controller.admin.unit.request.UnitAddCell;
 import com.windstorm.management.controller.admin.unit.request.UnitCreate;
+import com.windstorm.management.domain.cell.Cell;
 import com.windstorm.management.domain.unit.Unit;
+import com.windstorm.management.implement.cell.CellReader;
 import com.windstorm.management.repository.unit.UnitRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -13,10 +16,20 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UnitAppender {
 	private final UnitRepository unitRepository;
+	private final UnitReader unitReader;
+	private final CellReader cellReader;
 
 	@Transactional
 	public Unit append(UnitCreate request) {
 		Unit newUnit = Unit.create(request.division(), request.name());
 		return unitRepository.save(newUnit);
+	}
+
+	@Transactional
+	public void addCell(UnitAddCell request) {
+		Cell cell = cellReader.read(request.cellName());
+		Unit unit = unitReader.read(request.unitName());
+
+		unit.addCell(cell);
 	}
 }
--- src/main/java/com/windstorm/management/service/unit/UnitService.java
+++ src/main/java/com/windstorm/management/service/unit/UnitService.java
@@ -1,16 +1,12 @@
 package com.windstorm.management.service.unit;
 
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.windstorm.management.controller.admin.unit.request.UnitAddCell;
 import com.windstorm.management.controller.admin.unit.request.UnitCreate;
 import com.windstorm.management.controller.unit.response.UnitResponse;
-import com.windstorm.management.domain.cell.Cell;
 import com.windstorm.management.domain.unit.Unit;
-import com.windstorm.management.implement.cell.CellReader;
 import com.windstorm.management.implement.unit.UnitAppender;
-import com.windstorm.management.implement.unit.UnitReader;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,19 +14,13 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class UnitService {
 	private final UnitAppender unitAppender;
-	private final UnitReader unitReader;
-	private final CellReader cellReader;
 
 	public UnitResponse append(UnitCreate request) {
 		Unit newUnit = unitAppender.append(request);
 		return UnitResponse.toResponse(newUnit);
 	}
 
-	@Transactional
 	public void addCell(UnitAddCell request) {
-		Unit unit = unitReader.read(request.unitName());
-		Cell cell = cellReader.read(request.cellName());
-
-		unit.addCell(cell);
+		unitAppender.addCell(request);
 	}
 }
