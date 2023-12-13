41258192475d2c94eefe6e409954d46872df731e
hyunzxn <jann1653@gmail.com>
2023-12-13 17:38:39 +0900

    refactor: 멤버의 비밀번호를 바꾸는 메서드 이름 변경

--- src/main/java/com/windstorm/management/controller/member/MemberController.java
+++ src/main/java/com/windstorm/management/controller/member/MemberController.java
@@ -38,7 +38,7 @@ public class MemberController {
 	public ApiResponse<Object> modify(@AuthenticationPrincipal UserPrincipal userPrincipal,
 		@RequestBody PasswordModify request) {
 		String uniqueMemberId = userPrincipal.getUniqueId();
-		memberService.modify(uniqueMemberId, request);
+		memberService.modifyPassword(uniqueMemberId, request);
 		return ApiResponse.of(HttpStatus.OK, "비밀번호 변경이 완료됐습니다.", null);
 	}
 }
