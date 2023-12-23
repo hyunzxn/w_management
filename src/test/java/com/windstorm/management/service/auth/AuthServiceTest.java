58cda10f90ce0b30b7f42dbbad03ee765dfc223b
hyunzxn <jann1653@gmail.com>
2023-12-23 12:40:16 +0900

    chore: AuthServiceTest에 불필요한 import 제거

--- src/test/java/com/windstorm/management/service/auth/AuthServiceTest.java
+++ src/test/java/com/windstorm/management/service/auth/AuthServiceTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.ActiveProfiles;
 
