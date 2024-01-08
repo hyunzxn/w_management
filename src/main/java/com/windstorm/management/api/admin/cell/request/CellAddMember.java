aed2d997ae9fb36e07c85953184439aac729ff09
hyunzxn <jann1653@gmail.com>
2024-01-08 23:23:28 +0900

    feat: 새로운 Cell 추가할 때 청년부(Division)을 추가로 입력하도록 수정

--- src/main/java/com/windstorm/management/api/admin/cell/request/CellAddMember.java
+++ src/main/java/com/windstorm/management/api/admin/cell/request/CellAddMember.java
@@ -1,12 +1,18 @@
 package com.windstorm.management.api.admin.cell.request;
 
+import com.windstorm.management.domain.global.Division;
+
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record CellAddMember(
 	@NotBlank(message = "셀 이름을 입력해주세요.")
 	String cellName,
+
+	@NotNull(message = "청년부를 입력해주세요.")
+	Division division,
 
 	@NotBlank(message = "셀원 교적번호를 입력해주세요.")
 	String uniqueId
